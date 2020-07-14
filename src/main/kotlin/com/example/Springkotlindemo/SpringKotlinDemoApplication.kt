0cb06ae9a26ca288bb39f694934ed00d47dd9498
Sachin <sachinkmr375@gmail.com>
2020-07-14 19:08:22 +0530

    init repo

---
+++ src/main/kotlin/com/example/Springkotlindemo/SpringKotlinDemoApplication.kt
@@ -0,0 +1,15 @@
+package com.example.Springkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class SpringKotlinDemoApplication {
+
+}
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinDemoApplication>(*args)
+}
